17765fd22846caa9bc761e197153bff3d275f819
Saba Usanetashvili <usanetashvili.saba@gmail.com>
2024-09-01 02:41:06 +0400

    Added project files

---
+++ Data/Models/UserModels/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models.UserModels;
+
+public class User
+{
+    public int Id { get; set; }
+    [MaxLength(15)]
+    public string FirstName { get; set; } = null!;
+    [MaxLength(15)]
+    public string LastName { get; set; } = null!;
+    public Settings Settings { get; set; } = null!;
+    public Expenditure Expenditure { get; set; } = null!;
+    public Income Income { get; set; } = null!;
+    
+}
